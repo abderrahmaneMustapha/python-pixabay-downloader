ab7908c45537f1dadebe65ac35da9cd7da43ca49
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2019-11-22 12:44:05 +0100

    add simple kivy interface

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ import os
 import time
 import glob
 
+from kivy.app import App 
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+class Interface(Screen):
+    search = ObjectProperty(None)
+    pass
+
+kv = Builder.load_file("my.kv")
+
+class MainApp(App):
+    def build(self): 
+        return Interface()
+
+MainApp().run()
+
+"""
 start_time = time.time()
 
 rel_path_text_list = [f for f in glob.glob(r"pictures\\*.txt")]
@@ -14,3 +34,5 @@ for rel_path_text in rel_path_text_list:
         print(abs_file_path)
         print('true')
 print("Execution took : "+  str(time.time() - start_time) + " seconds")
+"""
+
