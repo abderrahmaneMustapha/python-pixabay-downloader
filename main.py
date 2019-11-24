81c2bd7dd3e08f2a3bd9c9fb68ace8df165030e0
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2019-11-24 23:31:42 +0100

    add image to interface

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.image import Image
 
 class Interface(Screen):
     search = ObjectProperty(None)
+    grid_layout = GridLayout(cols=4)
+    
+    def replace(self,string):
+        return string.replace(".txt", ".jpg")
+    def addtogrid(self,img):
+        self.grid_layout.add_widget(Image(source=img))
+        print(self.grid_layout)
+
     def find(self):
         start_time = time.time()
         #get the text from search text input
@@ -25,15 +35,20 @@ class Interface(Screen):
         for rel_path_text in rel_path_text_list:
             #get full path of txt document
             abs_file_path = os.path.join(script_dir, rel_path_text)
+           
             #get tags from txt file
             file_tags = open(abs_file_path, 'r').read()
             #search in text file
             if search_text in file_tags:
-                print(abs_file_path)
+                abs_image_file_path = self.replace(abs_file_path)
+                self.addtogrid( abs_image_file_path)
+                print(abs_image_file_path)                
                 print('true')
-        
+        root = self.ids.floatlayout.add_widget(self.grid_layout)
+        print(root)
         print("Execution took : "+  str(time.time() - start_time) + " seconds")
-        
+  
+    
 
 kv = Builder.load_file("my.kv")
 
@@ -43,18 +58,4 @@ class MainApp(App):
 
 MainApp().run()
 
-"""
-start_time = time.time()
-
-rel_path_text_list = [f for f in glob.glob(r"pictures\\*.txt")]
-
-script_dir  = os.path.dirname(os.path.abspath(__file__))
-for rel_path_text in rel_path_text_list:
-    abs_file_path = os.path.join(script_dir, rel_path_text)
-    file_tags = open(abs_file_path, 'r').read()
-    if "man" in file_tags:
-        print(abs_file_path)
-        print('true')
-print("Execution took : "+  str(time.time() - start_time) + " seconds")
-"""
 
